271c7d3eb1c25b7f3e982795f858b08e4465e7f7
Marc <dugue.marc1@gmail.com>
2025-05-12 18:12:07 +0200

    Cross origin fix

--- src/main/java/com/main/triviatreckapp/controller/RoomController.java
+++ src/main/java/com/main/triviatreckapp/controller/RoomController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-@CrossOrigin
+@CrossOrigin(origins = "https://triviatrek-back:8080")
 @Controller
 public class RoomController {
     private final RoomService roomService;
